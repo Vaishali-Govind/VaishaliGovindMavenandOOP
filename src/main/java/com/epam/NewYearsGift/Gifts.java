0f47f9169e772cab27ea82866a24d0eaa0adab39
vaishali govind <vaishali.govind99@gmail.com>
2020-02-03 22:31:43 +0530

    first commit

---
+++ src/main/java/com/epam/NewYearsGift/Gifts.java
@@ -0,0 +1,27 @@
+package com.epam.NewYearsGift;
+import Sweets.sweet;
+
+import java.util.*;
+
+public class Gifts {
+     
+	private int weight;
+	private List<sweet> Sweets = new ArrayList<>();
+	public void showGifts() {
+		for(sweet sweets : Sweets) {
+			System.out.println(sweets);
+		}
+	}
+	public void addGift(sweet sweets) {
+		Sweets.add(sweets);
+		this.weight += sweets.getWeight(); 
+	}
+	public void sortByWeight() {
+		 Collections.sort(Sweets);
+	        System.out.println(("Gifts after sorting by weight: "));
+	    }
+	
+	public int getTotalWeight() {
+			return this.weight;
+		}
+}
